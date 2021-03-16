3b265592eee572d54f7d09fddbb6434a499a509a
Alvin Togonon <artogalt@gmail.com>
2021-03-16 16:08:31 -0400

    update for COR

--- server/index.js
+++ server/index.js
@@ -11,6 +11,13 @@ app.use(volleyball)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
 // static middleware
 app.use(express.static(path.join(__dirname, '../public')))
 
